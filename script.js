94184b5649adebd88aee8aed91b8645fcb06695d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-08-30 13:08:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
+
+function compareValues(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+        outputBox.innerText="Your birthday is lucky 🥳"
+    }else{
+        outputBox.innerText="Your birthday is not lucky😥😥"
+    }
+}
+function checkBirthdayIsLucky(){
+    const dob=dateOfBirth.value;
+    const sum=calculateSum(dob);
+    if(sum&&dob)
+    compareValues(sum,luckyNumber.value)
+    else
+    outputBox.innerText="Please enter both the fields😡😡"
+}
+
+function calculateSum(dob){
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let index=0;index<dob.length;index++){
+          sum=sum + Number(dob.charAt(index));
+    }
+    return sum;
+
+}
+
+checkNumberButton.addEventListener('click',checkBirthdayIsLucky)
+   
